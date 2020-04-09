95910a18e389358c0f756c7c136d0e2fb8e92c8e
Keith Becker <kbecker@ehc.org>
2020-04-09 16:39:49 -0600

    working on php code

--- src/functions/utils.php
+++ src/functions/utils.php
@@ -4,6 +4,8 @@
 namespace utils;
 
 
+use Exception;
+
 function array_wrap_each(array $strItems, $before = '', $after = '') {
     $newItems = [];
 
@@ -11,6 +13,34 @@ function array_wrap_each(array $strItems, $before = '', $after = '') {
         $newItems[] = $before . $item . $after;
 
     return $newItems;
+}
+
+
+function echo_error_page(Exception $e)
+{
+    //generate error string to display to the user
+
+    $errorHeaders = ['Message','Stack Trace','File','Line','Code'];
+    $wrappedHeaders = array_wrap_each($errorHeaders, '<h2 class="error-header">', '</h2>');
+
+    $errorMessages = [$e->getMessage(),$e->getTraceAsString(),$e->getFile(),$e->getLine(),$e->getCode()];
+    $wrappedMessages = array_wrap_each($errorMessages, '<p class="error-message">', '</p>');
+
+    $errorMessage = '';
+
+    if (count($wrappedHeaders) !== count($wrappedMessages))
+        throw new Exception('count of each array should match');
+
+    for ($i = 0; $i < count($errorHeaders); $i++) {
+        $errorMessage .= "<div class='mb-4'>";
+        $errorMessage .= $wrappedHeaders[$i];
+        $errorMessage .= $wrappedMessages[$i];
+        $errorMessage .= "</div>";
+    }
+
+
+    include dirname(__FILE__) . '../html/error-message.php';
+
 }
 
 
--- src/main.php
+++ src/main.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__) . '/autoload.php';
 
 use Connect4\Game;
 use Connect4\Requests\Request;
-use function utils\array_wrap_each;
+use function utils\echo_error_page;
 
 
 //c++ style main function where the code begins
@@ -29,28 +29,7 @@ function main() {
 try {
     main();
 } catch (Exception $e) {
-    //generate error string to display to the user
-
-    $errorHeaders = ['Message','Stack Trace','File','Line','Code'];
-    $wrappedHeaders = array_wrap_each($errorHeaders, '<h2 class="error-header">', '</h2>');
-
-    $errorMessages = [$e->getMessage(),$e->getTraceAsString(),$e->getFile(),$e->getLine(),$e->getCode()];
-    $wrappedMessages = array_wrap_each($errorMessages, '<p class="error-message">', '</p>');
-
-    $errorMessage = '';
-
-    if (count($wrappedHeaders) !== count($wrappedMessages))
-        throw new Exception('count of each array should match');
-
-    for ($i = 0; $i < count($errorHeaders); $i++) {
-        $errorMessage .= "<div class='mb-4'>";
-        $errorMessage .= $wrappedHeaders[$i];
-        $errorMessage .= $wrappedMessages[$i];
-        $errorMessage .= "</div>";
-    }
-
-
-    include dirname(__FILE__) . '/html/error-message.php';
+    echo_error_page($e);
 }
 
 
