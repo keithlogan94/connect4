f53bf23531a85261f6d395118fd7c4f98fb508d3
Keith Becker <kbecker@ehc.org>
2020-04-11 14:07:15 -0600

    working on css

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,39 +1,58 @@
 
-
+$(document).on('click', '.place-checker', function () {
+    alert('placing checker');
+});
 
 
 $(function () {
 
-    $(document).on('click', '.place-checker', function () {
-        alert('placing checker');
-    });
+    $(".popup").hide();
+    startGame();
+
 
 
 });
 
 
+async function startGame()
+{
+    await createGame();
+    await loadGame(1, document.getElementById('game-container'));
+    $(".popup").show();
+
+}
+
+
+
 
 async function loadGame(gameId, containerElem) {
 
+    return new Promise(async function (resolve, reject) {
 
-    const boardPositions = await getGamePositions(gameId);
 
-    var html = '<div id="board">';
+        const boardPositions = await getGamePositions(gameId);
 
-    $.each(boardPositions, function (index, elem) {
+        var html = '<div id="board">';
 
-        var token = ``;
-        if (elem.is_filled)
-            token = `<div class="${elem.filled_color}"></div>`;
+        $.each(boardPositions, function (index, elem) {
 
-        html += `<div class="board-position" id="${elem.position_code}">
+            var token = ``;
+            if (elem.is_filled)
+                token = `<div class="${elem.filled_color}"></div>`;
+
+            html += `<div class="board-position" id="${elem.position_code}">
                     ${token}
                 </div>`;
-    });
+        });
 
-    html += '</div>';
+        html += '</div>';
 
-    $(containerElem).html(html);
+        $(containerElem).html(html);
+
+        resolve(true);
+
+
+    })
 
 }
 
