a1b8e09809d26200c7651ed07bd9ec53e4d91071
Keith Becker <kbecker@ehc.org>
2020-04-14 16:58:09 -0600

    auto load

--- src/html/basic-game-screen.php
+++ src/html/basic-game-screen.php
@@ -134,8 +134,8 @@
         src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
-<script src="/js/connect4.js"></script>
-<script src="/js/basic.js"></script>
+<script src="/js/connect4.js?ver=1.01"></script>
+<script src="/js/basic.js?ver=1.01"></script>
 </body>
 </html>
 
